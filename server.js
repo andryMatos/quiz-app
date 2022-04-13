28a49e7868a7476376b878e21df732ce46dc8ee5
AndryMatos <andry.matoscaamal@gmail.com>
2022-04-13 17:23:38 -0500

    Se agrega deploy heroku

--- server.js
+++ server.js
@@ -38,15 +38,12 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/quiz", quiz);
 
-if(process.env.NODE_ENV === 'production'){
-  
-  app.use(express.static('client/build'));
+app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client','build','index.html'))
-  });
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client','build','index.html'))
+});
 
-}
 
 const port = process.env.PORT || 5000;
 

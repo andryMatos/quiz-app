a60472cac5298bac85e6183e567f708bd74ed641
AndryMatos <andry.matoscaamal@gmail.com>
2022-04-13 17:13:52 -0500

    deploy heroku

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const passport = require("passport");
 const users = require("./routes/api/users");
 const quiz = require("./routes/api/quiz");
 const app = express();
+const path = require("path");
 
 // Bodyparser middleware
 app.use(
@@ -36,6 +37,16 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 app.use("/api/quiz", quiz);
+
+if(process.env.NODE_ENV === 'production'){
+  
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client','build','index.html'))
+  });
+
+}
 
 const port = process.env.PORT || 5000;
 
